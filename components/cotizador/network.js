3be27141022807c6359cc5a4ba7dd87d5c781884
Christian Cuevas <xhris.gab@gmail.com>
2022-03-09 10:52:24 -0400

    Modificado ruta POST para Cotizador y poder descargar el reporte PDF

--- components/cotizador/network.js
+++ components/cotizador/network.js
@@ -17,8 +17,11 @@ router.post('/',function(req, res){
     console.log(req.body.id);
     controller.reporteCotizador(req.body.id)
         .then((x)=>{
-            const stream = fs.createReadStream('public/proforma.pdf');
-            stream.pipe(res);
+            /*const stream = fs.createReadStream('public/proforma.pdf');
+            stream.pipe(res);*/
+            res.download('public/proforma.pdf',function(err){
+                console.log("Error: ", err);
+            })
         })
         .catch(err=> {
             console.error('[Error] Error Adenda', err);
